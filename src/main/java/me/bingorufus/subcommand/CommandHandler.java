b44cf9dd2a6ed0cb62a810ef0ca06e70e460ee74
BingoRufus <BingoRufus@gmail.com>
2021-01-21 12:03:26 -0800

    Updated gitignore

--- src/main/java/me/bingorufus/subcommand/CommandHandler.java
+++
@@ -1,8 +0,0 @@
-package me.bingorufus.subcommand;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface CommandHandler {
-     boolean execute(CommandSender sender, Command command,String label, String[] args);
-}
