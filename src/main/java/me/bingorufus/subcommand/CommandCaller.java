7b30948785f9d9eeafde3e27c21be8adfd0c1008
BingoRufus <BingoRufus@gmail.com>
2021-01-21 12:03:32 -0800

    Updated gitignore

---
+++ src/main/java/me/bingorufus/subcommand/CommandCaller.java
@@ -0,0 +1,57 @@
+package me.bingorufus.subcommand;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+class CommandCaller implements CommandExecutor {
+     private final SubCommand subCommand;
+     public CommandCaller(SubCommand subCommand) {
+         this.subCommand = subCommand;
+     }
+
+    @Override
+    public boolean onCommand( @Nonnull CommandSender sender, Command command, @Nonnull String label, @Nonnull String[] args) {
+         if(!command.getName().equalsIgnoreCase(subCommand.getCommandName())){
+             return true;
+         }
+         SubCommand applicableCommand = getSubCommand(subCommand,args);
+         if(applicableCommand == null){
+           sender.sendMessage( getSubCommand(subCommand,removeFirstItem(args)) == null ? getSubCommand(subCommand,removeFirstItem(args)) == null ? "/command" : Objects.requireNonNull(getSubCommand(subCommand, removeFirstItem(args))).getUsageMessage(): subCommand.getUsageMessage());
+             return true;
+         }
+         if(applicableCommand.getPermission() != null && !sender.hasPermission(applicableCommand.getPermission())){
+             sender.sendMessage(applicableCommand.getPermissionMessage());
+             return true;
+         }
+
+         if(applicableCommand.getCommandHandler() != null) {
+             if(!applicableCommand.getCommandHandler().execute(sender,command,label,args)) {
+                 sender.sendMessage(applicableCommand.getUsageMessage());
+             }
+         }
+
+        return true;
+    }
+    private SubCommand getSubCommand(SubCommand command, String[] args){
+         if(command.getSubCommands().size() == 0 || args.length == 0) return command;
+         for(SubCommand sub : command.getSubCommands()){
+             for(String alias : sub.getAliases().keySet()){
+                 if(alias.equalsIgnoreCase(args[0])){
+                     return getSubCommand(sub, removeFirstItem(args));
+                 }
+             }
+         }
+         return null;
+    }
+    private String[] removeFirstItem(String[] args){
+        ArrayList<String > out = new ArrayList<>(Arrays.asList(args));
+        out.remove(0);
+        return out.toArray(new String[0]);
+    }
+}
